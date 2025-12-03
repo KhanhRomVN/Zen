9b4a14132e76a41944a0fa59890ff1e8b2015762
KhanhRomVN <khanhromvn@gmail.com>
2025-12-03 18:45:26 +0700

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,94 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {}
+export class ZenChatViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "zen-chat";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [
+        vscode.Uri.joinPath(this._extensionUri, "src/webview-ui/dist"),
+        vscode.Uri.joinPath(this._extensionUri, "src/webview-ui/public"),
+      ],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "src/webview-ui/dist", "index.js")
+    );
+    const styleUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(
+        this._extensionUri,
+        "src/webview-ui/dist",
+        "index.css"
+      )
+    );
+    const nonce = this.getNonce();
+
+    return `<!DOCTYPE html>
+ <html lang="en">
+ <head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource} 'unsafe-inline'; script-src 'nonce-${nonce}';">
+ <link rel="stylesheet" href="${styleUri}">
+ <title>Zen Chat
+</title>
+ 
+</head>
+ <body>
+ <div id="root">
+</div>
+ <script nonce="${nonce}" src="${scriptUri}">
+</script>
+ 
+</body>
+ 
+</html>`;
+  }
+
+  private getNonce(): string {
+    let text = "";
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 32; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const provider = new ZenChatViewProvider(context.extensionUri);
+
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      ZenChatViewProvider.viewType,
+      provider
+    )
+  );
+
+  const openChatCommand = vscode.commands.registerCommand(
+    "zen.openChat",
+    () => {
+      vscode.commands.executeCommand("workbench.view.extension.zen-sidebar");
+    }
+  );
+
+  context.subscriptions.push(openChatCommand);
+}
 
 export function deactivate() {}
